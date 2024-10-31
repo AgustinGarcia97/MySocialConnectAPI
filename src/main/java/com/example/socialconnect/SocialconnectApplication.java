d3ae6bb48dbb138c2e85125f4b94144a5731a4a3
Agustin <aggarcia997@gmail.com>
2024-10-31 05:58:05 -0300

    Instalacion de dependecia Swagger para documentar la APIREST.
    Seteo de claves provisorias para seguridad

--- src/main/java/com/example/socialconnect/SocialconnectApplication.java
+++ src/main/java/com/example/socialconnect/SocialconnectApplication.java
@@ -3,7 +3,9 @@ package com.example.socialconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class SocialconnectApplication {
 
 	public static void main(String[] args) {
